f8930ff979dcc77cc4596a493be30653028027e5
ZachariahJudd <zjudd1957@greenvilleschools.us>
2021-02-23 18:10:30 +0000

    Initial commit

---
+++ application.py
@@ -0,0 +1,20 @@
+import os
+from flask import url_for
+import models
+import routes
+
+application = app = routes.app
+
+if __name__ == '__main__':
+    models.initialize()
+    try:
+        models.User.create_user(
+            full_name='ZachariahJudd',
+            email='juddcraft12@gmail.com',
+            password='Zachj9605!',
+            mobile_no='8644693128',
+            admin=True
+        )
+    except ValueError:
+        pass
+    application.run(debug = True, host='0.0.0.0', threaded=True)
